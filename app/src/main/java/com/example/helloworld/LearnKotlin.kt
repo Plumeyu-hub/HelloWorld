b76d27e0b9923f79f8c0f8b25ab47814d82c6265
Plumeyu-hub <1404300825@qq.com>
2020-08-07 17:59:15 +0800

    Kt程序的逻辑控制-if条件语句

--- app/src/main/java/com/example/helloworld/LearnKotlin.kt
+++ app/src/main/java/com/example/helloworld/LearnKotlin.kt
@@ -1,7 +1,5 @@
 package com.example.helloworld
 
-import java.lang.Math.max
-
 /**
  * 学习Kotlin
  * @author wangshy
@@ -16,7 +14,39 @@ fun main() {
 }
 
 //fun largerNumber(num1: Int, num2: Int): Int {
-//    return max(num1, num2)
+//    var value = 0
+//    if (num1 > num2) {
+//        value = num1
+//    } else {
+//        value = num2
+//    }
+//    return value
 //}
-//fun largerNumber(num1: Int, num2: Int): Int = max(num1, num2)
-fun largerNumber(num1: Int, num2: Int) = max(num1, num2)
+
+//Kotlin中的if语句相比于Java有一个额外的功能，它是可以有返回值的，返回值就是if语句每一个条件中最后一行代码的返回值。
+//fun largerNumber(num1: Int, num2: Int): Int {
+//    val value = if (num1 > num2) {
+//        num1
+//    } else {
+//        num2
+//    }
+//    return value
+//}
+
+//value是一个多余的变量，可以直接将if语句返回
+//fun largerNumber(num1: Int, num2: Int): Int {
+//    return if (num1 > num2) {
+//        num1
+//    } else {
+//        num2
+//    }
+//}
+
+//语法糖:当一个函数只有一行代码时，可以省略函数体部分，直接将这一行代码使用等号串连在函数定义的尾部。
+//fun largerNumber(num1: Int, num2: Int) = if (num1 > num2) {
+//    num1
+//} else {
+//    num2
+//}
+
+fun largerNumber(num1: Int, num2: Int) = if (num1 > num2) num1 else num2
