cb3f7dfd262304dc074d9af7ecfcff368783345f
Plumeyu-hub <1404300825@qq.com>
2020-08-07 16:09:12 +0800

    输出不可变的变量

--- app/src/main/java/com/example/helloworld/LearnKotlin.kt
+++ app/src/main/java/com/example/helloworld/LearnKotlin.kt
@@ -7,5 +7,8 @@ package com.example.helloworld
  */
 
 fun main(){
-    println("Hello Kotlin!")
+    //val（value的简写）用来声明一个不可变的变量,对应Java里的final变量
+    //Kotlin有类型推导机制，但是如果对一个变量延迟赋值，Kotlin就无法自动推导它的类型
+    val a=10
+    println("a="+a)
 }
