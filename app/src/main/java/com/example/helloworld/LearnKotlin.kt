79bf1d2148d93cef54b07f692a435b7002e49713
Plumeyu-hub <1404300825@qq.com>
2020-08-07 17:22:46 +0800

    Kt函数

--- app/src/main/java/com/example/helloworld/LearnKotlin.kt
+++ app/src/main/java/com/example/helloworld/LearnKotlin.kt
@@ -1,14 +1,22 @@
 package com.example.helloworld
 
+import java.lang.Math.max
+
 /**
  * 学习Kotlin
  * @author wangshy
  * @date 2020/08/07
  */
 
-fun main(){
-    //val（value的简写）用来声明一个不可变的变量,对应Java里的final变量
-    //Kotlin有类型推导机制，但是如果对一个变量延迟赋值，Kotlin就无法自动推导它的类型
-    val a=10
-    println("a="+a)
+fun main() {
+    val a = 37
+    val b = 40
+    val value = largerNumber(a, b)
+    println("larger number is " + value)
 }
+
+//fun largerNumber(num1: Int, num2: Int): Int {
+//    return max(num1, num2)
+//}
+//fun largerNumber(num1: Int, num2: Int): Int = max(num1, num2)
+fun largerNumber(num1: Int, num2: Int) = max(num1, num2)
