341f4916a3a03a6c5d13a1a3b86b1f2587910052
Plumeyu-hub <1404300825@qq.com>
2020-08-14 14:42:04 +0800

    Kt程序的逻辑控制-when条件语句

--- app/src/main/java/com/example/helloworld/LearnKotlin.kt
+++ app/src/main/java/com/example/helloworld/LearnKotlin.kt
@@ -7,10 +7,19 @@ package com.example.helloworld
  */
 
 fun main() {
-    val a = 37
-    val b = 40
-    val value = largerNumber(a, b)
-    println("larger number is " + value)
+//    val a = 37
+//    val b = 40
+//    val value = largerNumber(a, b)
+//    println("larger number is " + value)
+
+    //println(getScore("Jim"));
+
+//    val num = 10
+//    checkNumber(num)
+
+    val num = 10L
+    checkNumber(num)
+
 }
 
 //fun largerNumber(num1: Int, num2: Int): Int {
@@ -49,4 +58,43 @@ fun main() {
 //    num2
 //}
 
-fun largerNumber(num1: Int, num2: Int) = if (num1 > num2) num1 else num2
+//fun largerNumber(num1: Int, num2: Int) = if (num1 > num2) num1 else num2
+
+//fun getScore(name: String) = if (name == "Tom") {
+//    86
+//} else if (name == "Jim") {
+//    77
+//} else if (name == "Jack") {
+//    95
+//} else if (name == "Lily") {
+//    100
+//} else {
+//    0
+//}
+
+fun getScore(name: String) = when (name) {
+    "Tom" -> 86
+    "Jim" -> 77
+    "Jack" -> 95
+    "Lily" -> 100
+    else -> 0
+}
+
+fun getScore1(name: String) = when {
+    //不管你传入的名字是Tom还是Tommy，只要是以Tom开头的名字，他的分数就是86分
+    name.startsWith("Tom") -> 86
+    name == "Jim" -> 77
+    name == "Jack" -> 95
+    name == "Lily" -> 100
+    else -> 0
+}
+
+
+fun checkNumber(num: Number) {
+    when (num) {
+        is Int -> println("Int")
+        is Double -> println("Double")
+        else -> println("number not support")
+    }
+}
+
